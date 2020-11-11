ded1451ae939a75c4e98d57b4d11350890ba75be
zhaofeng-shu33 <616545598@qq.com>
2020-11-11 12:16:16 +0800

    finish first test

--- kmeans.py
+++ kmeans.py
@@ -21,12 +21,12 @@ class KMeans:
         -------
         None
         """
-
+        inner_x_train = x_train.astype('float')
         num_of_data = x_train.shape[0]
         dim = x_train.shape[-1] # dimension of features
         # we take initial centroid to be randomly sampled points
         centroid_index  = np.random.choice(np.arange(num_of_data), size=self.n_clusters, replace=False)
-        cluster_centers_  = x_train[centroid_index, :]
+        cluster_centers_  = inner_x_train[centroid_index, :]
 
         S = np.zeros(num_of_data, dtype=int)
         last_cluster_centers_ = np.zeros([self.n_clusters, dim])
@@ -34,17 +34,23 @@ class KMeans:
         while np.linalg.norm(cluster_centers_ - last_cluster_centers_) >= self.tol \
             and iteration_cnt < self.max_iter:
             last_cluster_centers_ = cluster_centers_.copy()
-            #***********Assignment***************
+            #*********** Assignment ***************
             for i in range(num_of_data):
-                S[i] = np.argmin(np.linalg.norm(x_train[i, :] - cluster_centers_, axis=1))
-            #***********Update******************
+                S[i] = np.argmin(np.linalg.norm(inner_x_train[i, :] - cluster_centers_, axis=1))
+            #*********** Update ******************
             for i in range(self.n_clusters):
                 index_list = np.where(S==i)[0]
-                if not len(index_list) == 0:                    
-                    cluster_centers_[i, :] = np.mean(x_train[index_list, :], axis=0)
+                if not len(index_list) == 0:
+                    cluster_centers_[i, :] = np.mean(inner_x_train[index_list, :], axis=0)
             iteration_cnt += 1
         self.cluster_centers_ = cluster_centers_
         self.labels_ = S
+        # compute loss here
+        loss = 0
+        for i in range(num_of_data):
+            current_centroid = cluster_centers_[S[i], :]
+            loss += np.linalg.norm(inner_x_train[i, :] - current_centroid) ** 2
+        self.inertia_ = loss
 
     def fit(self, x_train):
         # alias for train
--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import unittest
 
 import numpy as np
 from sklearn import metrics
+from sklearn.utils._testing import assert_array_almost_equal
 
 from kmeans import KMeans
 
@@ -11,7 +12,14 @@ class TestKMeans(unittest.TestCase):
         kmeans = KMeans(2)
         np.random.seed(2020)
         kmeans.fit(x)
+        self.assertAlmostEqual(kmeans.inertia_, 1.0)
         self.assertAlmostEqual(metrics.adjusted_rand_score(kmeans.labels_, [0, 0, 1, 1]), 1.0)
+        if np.abs(kmeans.cluster_centers_[0, 0] - 4) < 1e-5:
+            assert_array_almost_equal(kmeans.cluster_centers_, np.array([[4, 0.5], [0, 0.5]]))
+        else:
+            assert_array_almost_equal(kmeans.cluster_centers_, np.array([[0, 0.5], [4, 0.5]]))
+    def test_gaussian_mixture(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
