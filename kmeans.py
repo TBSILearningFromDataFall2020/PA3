c8c8fdf15c98c3522bb210d36f586a785bde91f5
zhaofeng-shu33 <616545598@qq.com>
2020-12-07 13:22:48 +0800

    add answer

--- kmeans.py
+++ kmeans.py
@@ -35,9 +35,36 @@ class KMeans:
         None
         """
         # modify the following code
-        self.cluster_centers_ = np.zeros([self.n_clusters, x_train.shape[-1]])
-        self.labels_ = np.zeros(x_train.shape[0], dtype=int)
-        self.inertia_ = 0
+        inner_x_train = x_train.astype('float')
+        num_of_data = x_train.shape[0]
+        dim = x_train.shape[-1] # dimension of features
+        # we take initial centroid to be randomly sampled points
+        centroid_index  = np.random.choice(np.arange(num_of_data), size=self.n_clusters, replace=False)
+        cluster_centers_  = inner_x_train[centroid_index, :]
+
+        S = np.zeros(num_of_data, dtype=int)
+        last_cluster_centers_ = np.zeros([self.n_clusters, dim])
+        iteration_cnt = 0
+        while np.linalg.norm(cluster_centers_ - last_cluster_centers_) >= self.tol \
+            and iteration_cnt < self.max_iter:
+            last_cluster_centers_ = cluster_centers_.copy()
+            #*********** Assignment ***************
+            for i in range(num_of_data):
+                S[i] = np.argmin(np.linalg.norm(inner_x_train[i, :] - cluster_centers_, axis=1))
+            #*********** Update ******************
+            for i in range(self.n_clusters):
+                index_list = np.where(S==i)[0]
+                if not len(index_list) == 0:
+                    cluster_centers_[i, :] = np.mean(inner_x_train[index_list, :], axis=0)
+            iteration_cnt += 1
+        self.cluster_centers_ = cluster_centers_
+        self.labels_ = S
+        # compute loss here
+        loss = 0
+        for i in range(num_of_data):
+            current_centroid = cluster_centers_[S[i], :]
+            loss += np.linalg.norm(inner_x_train[i, :] - current_centroid) ** 2
+        self.inertia_ = loss
         # end of your modification
 
     def fit(self, x_train):
--- spectral_clustering.py
+++ spectral_clustering.py
@@ -25,7 +25,7 @@ class SpectralClustering:
     """
     def __init__(self, n_clusters, gamma=1.0):
         self.n_clusters = n_clusters
-        self.skip = True # modify it to False for bonus question
+        self.skip = False # modify it to False for bonus question
         self.gamma = gamma
 
     def train(self, x_train):
@@ -54,7 +54,16 @@ class SpectralClustering:
             np.array, shape (num_samples, num_samples)
         '''
         # start of your modification
-        return np.ones([x_train.shape[0], x_train.shape[0]])
+        # construct affinity matrix
+        n = x_train.shape[0] # num_data
+        m = x_train.shape[-1] # num_features
+        cross_ = x_train @ x_train.T
+        cross_diag = np.diag(cross_)
+        all_one_v = np.ones([n])
+        square_mat = np.kron(all_one_v, cross_diag).reshape([n, n])
+        square_mat += np.kron(cross_diag, all_one_v).reshape([n, n])
+        square_mat -= 2 * cross_
+        return np.exp(-self.gamma * square_mat)
         # end of your modification
 
     def _get_embedding(self, norm_laplacian=False):
@@ -74,7 +83,21 @@ class SpectralClustering:
             np.array, shape (num_samples, k)
         '''
         # start of your modification
-        return np.ones([self.affinity_matrix_.shape[0], self.n_clusters])
+        n = self.affinity_matrix_.shape[0]
+        # compute the unnormalized Laplacian
+        D = np.sum(self.affinity_matrix_, axis=0)
+        L =  np.diag(D) - self.affinity_matrix_
+        if norm_laplacian:
+            m = np.array(self.affinity_matrix_)
+            np.fill_diagonal(m, 0)
+            D = np.sum(m, axis=0)
+            L = np.eye(D.shape[0]) - np.diag(1.0 / D) @ m
+        values, vectors = np.linalg.eig(L)
+        Ls = [[i, values[i]] for i in range(n)]
+        Ls.sort(key=lambda x:x[1])
+        k = self.n_clusters
+        selected_array = [Ls[i][0] for i in range(k)]
+        return vectors[:, selected_array]
         # end of your modification
 
     def fit(self, x_train):
